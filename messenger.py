ba94f50eea2f7748252890c68fc99b142678645a
ian.bracken@duke.edu <idb15@duke.edu>
2024-10-28 22:41:41 -0400

    signCert implementation

--- messenger.py
+++ messenger.py
@@ -1,6 +1,10 @@
 import os
 import pickle
 import string
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 class MessengerServer:
     def __init__(self, server_signing_key, server_decryption_key):
@@ -8,12 +12,26 @@ class MessengerServer:
         self.server_decryption_key = server_decryption_key
 
     def decryptReport(self, ct):
-        raise Exception("not implemented!")
-        return
+        # Decrypts and returns an abuse report using server_decryption_key
+        # - Reported messages are encrypted with a CCA-secure variant of El-Gamal encryption.
+        # - El-Gamal encryption is not provided by the cryptography library.
+        # - We will implement it using available primitives (ECDH and AES-GCM).
+        ct_dict = pickle.loads(ct)
 
     def signCert(self, cert):
-        raise Exception("not implemented!")
-        return
+        # Signs a certificate that is provided using server_signing_key
+        # Signs the certificate with an ECDSA signature using SHA256
+        
+        # Serialize the certificate to bytes (e.g., using pickle)
+        cert_bytes = pickle.dumps(cert)
+
+        signature = self.server_signing_key.sign(
+            cert_bytes,
+            ec.ECDSA(hashes.SHA256())
+        )
+
+        # Return the signature
+        return signature
 
 class MessengerClient:
 
@@ -38,11 +56,12 @@ class MessengerClient:
         return
 
     def sendMessage(self, name, message):
-        # 
+        # Send an encrypted message to the user specified by 'name'
         raise Exception("not implemented!")
         return
 
     def receiveMessage(self, name, header, ciphertext):
+        # Receive and decrypt an encrypted message from the user specified by 'name'.
         raise Exception("not implemented!")
         return
 
@@ -51,7 +70,6 @@ class MessengerClient:
         # Encrypt the report under the server;s public key
         # Ensure the report includes the sender's name and message content
         # Ensure the report includes the sender's name and message content 
+        # This is sent and decrypted by the server(I believe)
         raise Exception("not implemented!")
         return
-
-# AHHHHH
