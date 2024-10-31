8c096fff315c04f040c59ba09866ed829f4a5073
ian.bracken@duke.edu <idb15@duke.edu>
2024-10-30 22:33:56 -0400

    session_update rework and receiveMessage implementated

--- messenger.py
+++ messenger.py
@@ -41,7 +41,9 @@ class MessengerClient:
         self.name = name
         self.server_signing_pk = server_signing_pk
         self.server_encryption_pk = server_encryption_pk
+        # Connections
         self.conns = {}
+        # Certificates
         self.certs = {}
         # Probably better here than in generateCertificate
         # Docs for SECP256R1(P-256): https://cryptography.io/en/latest/hazmat/primitives/asymmetric/ec/ (About 1/3 of the way down the page, we're using 256 instead of 384)
@@ -82,8 +84,7 @@ class MessengerClient:
     def sendMessage(self, name, message):
         # Send an encrypted message to the user specified by 'name'
         if name not in self.conns:
-            # TODO session_init
-            self.session_init(name)
+            self.session_init(name, True)
         
         session = self.conns[name]
 
@@ -113,6 +114,8 @@ class MessengerClient:
 
         #  Message number incremented
         session['Ns'] += 1
+        # Sending chain key updated
+        session['CKs'] = CKs
 
         # Prepare ciphertext components as hex strings for JSON serialization
         ciphertext_dict = {
@@ -124,8 +127,63 @@ class MessengerClient:
 
     def receiveMessage(self, name, header, ciphertext):
         # Receive and decrypt an encrypted message from the user specified by 'name'.
-        raise Exception("not implemented!")
-        return
+        if name not in self.conns:
+            self.session_init(name,False)
+        session = self.conns[name]
+
+        # Deserialize the header
+        serialized_header = pickle.dumps(header)
+
+        # Sender's DH public key from header
+        DHr_sender = header['dh_pub']
+
+        # If the sender's public key doesn't match the one stored in the session; different sender
+        # Time to enable to enable the ratchet
+        if not DHr_sender.public_numbers() == session['DHr'].public_numbers():
+            # Update previous message chain length with 'current' chain length(since it is a new chain)
+            session['Pn'] = session['Ns']
+            session['Ns'] = 0
+            # Reset Chain Keys(new chain)
+            session['CKs'] = None
+            # Update the stored public key
+            prev_DHr = session['DHr']
+            session['DHr'] = DHr_sender
+
+            # Computes a shared secret via DH key exchange ECDH - Elliptic Curve Diffie Hellman
+            shared_secret = session['DHs'].exchange(ec.ECDH(), DHr_sender)
+            # updates root key and receiving key for forward secrecy
+            session['RK'], session['CKr'] = self.KDF_RK(session['RK'], shared_secret)
+
+            # New sending key
+            session['DHs'] = ec.generate_private_key(ec.SECP256R1())
+
+            # Computes a shared secret via DH key exchange ECDH - Elliptic Curve Diffie Hellman
+            shared_secret = session['DHs'].exchange(ec.ECDH(), DHr_sender)
+            # updates root key and sending chain key
+            session['RK'], session['CKs'] = self.KDF_RK(session['RK'], shared_secret)
+        
+        # Nothing to receive
+        if session['CKr'] is None:
+            return None
+        
+        # Derive new Receiving Chain Key (CKr) and Message Key (MK) Unique from MK used when sending
+        CKr, MK = self.KDF_CK(session['CKr'])
+        session['CKr'] = CKr
+
+        # Decrypt the message using MK
+        aesgcm = AESGCM(MK)
+        try:
+            plaintext = aesgcm.decrypt(ciphertext['nonce'], ciphertext['ciphertext'], serialized_header)
+            message = plaintext.decode('utf-8')
+        except Exception as e:
+            # Tampering detected
+            return None
+
+        # Update Nr number received
+        session['Nr'] += 1
+
+        return message
+
 
     def report(self, name, message):
         # Implement El-Gamal encryption for abuse report
@@ -137,23 +195,20 @@ class MessengerClient:
     
     def session_init(self, name):
         peer_cert = self.certs[name]
-        # Other person's publick key
         DHr = peer_cert['public_key']
         DHs = self.DH_priv
 
-        # Compute shared secret
+        # Compute shared secret using ECDH
         shared_secret = self.DH_priv.exchange(ec.ECDH(), DHr)
 
-        # Root key
-        initial_RK = b'\x00' * 32
+        # Derive the initial Root Key and both Chain Keys from the shared secret
+        initial_RK, initial_CKs = self.KDF_RK(None, shared_secret)
 
-        session_RK, CKs = self.KDF_RK(initial_RK, shared_secret)
-
-        # Initialize session state
+        # Initialize session state with both sending and receiving chain keys
         session = {
-            'RK': session_RK,
-            'CKs': CKs,
-            'CKr': None,
+            'RK': initial_RK,
+            'CKs': initial_CKs,    # Sending Chain Key
+            'CKr': initial_CKs,    # Receiving Chain Key
             'DHs': DHs,
             'DHr': DHr,
             'Ns': 0,
@@ -162,6 +217,8 @@ class MessengerClient:
         }
 
         self.conns[name] = session
+
+
 
     def KDF_CK(self,CK):
         # From Docs 5.2
@@ -189,7 +246,7 @@ class MessengerClient:
         # using rk as the salt, dh_out as the input keying material (IKM), and an application-specific byte sequences as HKDF info.
         # Info value should be distinct from other uses of HKDF
 
-        # Is info distinct?
+        # Is info usage distinct?
         # I believe so because all the calls to the function are for the same purpose of
         # deriving new Root Key and Chain Key
         info = b'DoubleRatchetKDF_RK'
@@ -202,7 +259,8 @@ class MessengerClient:
         )
 
         derived_keys = hkdf.derive(DH_out)
+        # Everything up to 32 bytes is the next Root Key
         next_RK = derived_keys[:32]
         CKs = derived_keys[32:]
-    
+        # Return the next Root Key and Chain Key
         return next_RK, CKs
